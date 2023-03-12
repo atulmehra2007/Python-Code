bd9ea153909e48588dd1efbecd55ef57d5cd7703
atulmehra2007 <101255469+atulmehra2007@users.noreply.github.com>
2023-03-12 17:43:33 +0530

    Update class.py

--- class.py
+++ class.py
@@ -1,7 +1,7 @@
 ass student:
     school='KV-2'
     def name(self):
-        print (' Student name -> ',self.school)
-        print('School name -> ')
+       
+        print('School name -> ',self.school)
 student1= student()
 student1.name()
